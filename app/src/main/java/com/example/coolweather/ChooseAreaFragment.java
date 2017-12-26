c26c04efe9344ad413c67ab7da4e94839f40d1e9
wodexiaoming <863915018@qq.com>
2017-12-26 21:28:34 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,8 @@
+package com.example.coolweather;
+
+/**
+ * Created by tzq on 2017/12/22.
+ */
+
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -0,0 +1,8 @@
+package com.example.coolweather.util;
+
+/**
+ * Created by tzq on 2017/12/21.
+ */
+
+public class Utility {
+}
